73fe2133b3514b249e2c9498c70621e7bc4777d1
Mario <A01732529@tec.mx>
2022-11-06 20:22:36 -0600

    Removed unnecessary png images. Modified act.1.3 to give time

--- act.1.3/openmp_hilos.c
+++ act.1.3/openmp_hilos.c
@@ -5,10 +5,15 @@
 int main ()
 {
   omp_set_num_threads(NUM_THREADS);
+  double t1 = omp_get_wtime();
   #pragma omp parallel
   {
     int ID = omp_get_thread_num();
     printf("ID procesador(%d)\n", ID);
   }
+  double t2 = omp_get_wtime();
+  double time = t2 - t1;
+
+  printf("tomo %lf segundos\n", time);
   return 0;
 }
